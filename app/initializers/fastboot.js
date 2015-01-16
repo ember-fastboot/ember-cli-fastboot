08918b2d80515f5046f0f9c7dd311f8aafb53c2f
Tom Dale and Yehuda Katz <engineering+tomdale+wycats@tilde.io>
2015-01-15 19:37:51 -0800

    Hacks-on-hacks express<->ember routing connected
    This commit does some particularly egregious hacks to get the Express
    router and the Ember router working together. In particular, though it
    ostensibly works, please be careful not to make two requests in parallel
    as this will cause the templates to blow up.
    
    This code is not long for this earth, but helps us get a better sense of
    what changes are required for a proper solution.

--- app/initializers/fastboot.js
+++ app/initializers/fastboot.js
@@ -1,5 +1,16 @@
 /*globals SimpleDOM, Ember, FastBoot*/
 
+function resetTemplateState(container) {
+  var cache = container.cache;
+
+  Object.keys(cache).forEach(function(key) {
+    if (key.match(/^template:/)) {
+      cache[key].cachedFragment = null;
+      cache[key].hasRendered = false;
+    }
+  });
+}
+
 export default {
   name: "fast-boot",
 
@@ -8,6 +19,9 @@ export default {
     if (typeof document === 'undefined') {
       var doc = new SimpleDOM.Document();
 
+      // This needs to be setting up renderer:main, and ideally would have a less hacked
+      // up interface. In particular, the only ACTUAL swap-in here is the fake document,
+      // so it would be nice if we could register just that.
       registry.register('renderer:-dom', {
         create: function() {
           return new Ember.View._Renderer(new Ember.View.DOMHelper(doc));
@@ -16,21 +30,50 @@ export default {
 
       Ember.View.reopen({
         appendTo: function() {
-          var morph = {
+          // We should not need to fake out a morph to get the DOM
+          var morph = FastBoot.morph = {
             contextualElement: {},
             setContent: function(element) {
               this.element = element;
-            }
+            },
+
+            destroy: function() { }
           };
 
           this._morph = morph;
 
-          this.renderer.renderTree(this);
+          var app = this.container.lookup('application:main');
 
-          var serializer = new SimpleDOM.HTMLSerializer(SimpleDOM.voidMap);
-          var serialized = serializer.serialize(morph.element);
+          // This should be part of the appInstance.visit API
+          Ember.run(this, function() {
+            this.renderer.renderTree(this);
+          });
 
-          FastBoot.resolve(serialized);
+          var container = this.container;
+
+          FastBoot.resolve(function(url) {
+
+            // The app should expose a handleURL that returns a promise with the correct info
+            return app.__container__.lookup('router:main').handleURL(url).promise.then(function() {
+              return new Ember.RSVP.Promise(function(res) {
+                // This setTimeout is due to the fact that the router's promise doesn't include
+                // a wait for the template to finish rendering. We should add something for this.
+                setTimeout(function() {
+                  var serializer = new SimpleDOM.HTMLSerializer(SimpleDOM.voidMap);
+                  res(serializer.serialize(FastBoot.morph.element));
+
+                  // This is to avoid the templates calling cloneNode on the cached fragment on
+                  // the second pass through an HTMLBars template.
+                  resetTemplateState(container);
+
+                  // This should be replaced by proper support for application instances.
+                  app.buildRegistry();
+                  app.reset();
+
+                }, 500);
+              });
+            });
+          });
         }
       });
     }
