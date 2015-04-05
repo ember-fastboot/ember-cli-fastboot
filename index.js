6874505c615f66455e17a6cd1a1f682f145bcfb5
gdub22 <garth22@gmail.com>
2015-04-05 12:16:21 -0400

    enable content-for from other addons

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ module.exports = {
     return {
       fastboot: require('./lib/commands/fastboot')
     };
+  },
+
+  contentFor: function(type, config) {
+    // When using a dist built html file, we need to reinsert 
+    // content-for body so fastboot can insert its content.
+    if (type === 'body') {
+      return "{{content-for 'body'}}";
+    }
   }
 };
