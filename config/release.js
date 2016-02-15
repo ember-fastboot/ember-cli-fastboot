7b469abe49ac331eccd3bd18b3cbdf93d71d5299
Tom Dale <tom@tomdale.net>
2016-02-15 11:24:34 -0500

    Add ember-cli-release

---
+++ config/release.js
@@ -0,0 +1,20 @@
+/* jshint node:true */
+// var RSVP = require('rsvp');
+
+// For details on each option run `ember help release`
+module.exports = {
+  // local: true,
+  // remote: 'some_remote',
+  // annotation: "Release %@",
+  // message: "Bumped version to %@",
+  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
+  // strategy: 'date',
+  // format: 'YYYY-MM-DD',
+  // timezone: 'America/Los_Angeles',
+  //
+  // beforeCommit: function(project, versions) {
+  //   return new RSVP.Promise(function(resolve, reject) {
+  //     // Do custom things here...
+  //   });
+  // }
+};
