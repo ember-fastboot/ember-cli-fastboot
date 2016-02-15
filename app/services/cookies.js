91ab5907a09f54384141de18da44eccabea2f25b
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-02-15 15:59:50 +0100

    add cookies service

---
+++ app/services/cookies.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  // TODO: implement cookies access as wrapper around either request/response (as set by ember-fastboot-server) if in fastboot mode
+  // or document.cookie if in browser mode
+});
