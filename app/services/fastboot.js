013fb7dbcd3d4338436ffcbd10d0fa67dd50f3a3
Dan McClain <git@danmcclain.net>
2016-02-27 16:09:29 -0500

    Introduce API for current host
    This makes the host of the current request available from within the
    fastboot service

--- app/services/fastboot.js
+++ app/services/fastboot.js
@@ -1,8 +1,12 @@
 import Ember from "ember";
 
 let alias = Ember.computed.alias;
+let computed = Ember.computed;
 
 export default Ember.Service.extend({
   cookies: alias('_fastbootInfo.cookies'),
-  headers: alias('_fastbootInfo.headers')
+  headers: alias('_fastbootInfo.headers'),
+  host: computed(function() {
+    return this._fastbootInfo.host();
+  })
 });
