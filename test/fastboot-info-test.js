02b8ddad630402651955f22bd6f431b42c3d7518
Arjan Singh <arjan.singh@dollarshaveclub.com>
2016-04-28 09:50:51 -0700

    Add FastBootResponse Helper (#47)
    * Add FastBootResponse helper
    
    * Pass FastBootResponse headers from AppInstance to Server

--- test/fastboot-info-test.js
+++ test/fastboot-info-test.js
@@ -1,11 +1,17 @@
 var expect = require('chai').expect;
 var path = require('path');
 var FastBootInfo = require('../lib/fastboot-info.js');
+var FastBootResponse = require('../lib/fastboot-response.js');
+var FastBootRequest = require('../lib/fastboot-request.js');
 
 describe("FastBootInfo", function() {
-  it("has a FastBootRequest", function() {
-    var response = {};
-    var request = {
+  var response;
+  var request;
+  var fastbootInfo;
+
+  beforeEach(function () {
+    response = {};
+    request = {
       cookie: "",
       protocol: "http",
       headers: {
@@ -14,9 +20,16 @@ describe("FastBootInfo", function() {
         return this.cookie;
       }
     };
-    var fastbootInfo = new FastBootInfo(request, response);
 
-    expect(fastbootInfo.request.protocol).to.equal("http");
+    fastbootInfo = new FastBootInfo(request, response);
+  });
+
+  it("has a FastBootRequest", function() {
+    expect(fastbootInfo.request).to.be.an.instanceOf(FastBootRequest);
+  });
+
+  it("has a FastBootResponse", function() {
+    expect(fastbootInfo.response).to.be.an.instanceOf(FastBootResponse);
   });
 });
 
---
+++ test/fastboot-response-test.js
@@ -0,0 +1,47 @@
+var expect = require('chai').expect;
+var path = require('path');
+var FastBootHeaders = require('../lib/fastboot-headers.js');
+var FastBootResponse = require('../lib/fastboot-response.js');
+
+describe("FastBootResponse", function() {
+  var fastBootResponse;
+
+  beforeEach(function () {
+    var mockResponse = {
+      headers: {
+        "i-am-a": "mock header, me too"
+      },
+      get: function () {
+        return this.headers.cookie;
+      }
+    }
+
+    fastBootResponse = new FastBootResponse(mockResponse);
+  });
+
+  describe("headers", function () {
+    it("should have headers", function () {
+      expect(fastBootResponse).to.have.ownProperty("headers");
+    });
+
+    it("should be an instance of FastBootHeaders", function () {
+      expect(fastBootResponse.headers).to.be.an.instanceOf(FastBootHeaders);
+    });
+
+    it("should contain the original response's headers", function () {
+      var header = fastBootResponse.headers.getAll("i-am-a");
+      expect(header).to.deep.equal(["mock header", "me too"]);
+    });
+  });
+
+  describe("statusCode", function () {
+    it("should have a statusCode", function () {
+      expect(fastBootResponse).to.have.ownProperty("statusCode");
+    });
+
+    it("should default to 200", function () {
+      expect(fastBootResponse.statusCode).to.equal(200);
+    });
+  });
+});
+
