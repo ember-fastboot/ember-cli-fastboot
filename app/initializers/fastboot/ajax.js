ceecbf5b8fafe5f50ee581418a6a3fceecd6b6fa
Dan McClain <git@danmcclain.net>
2016-04-28 15:04:53 -0400

    Handle protocol relative URLs in _ajaxRequest injection (#186)

--- app/initializers/fastboot/ajax.js
+++ app/initializers/fastboot/ajax.js
@@ -4,11 +4,15 @@ import Ember from 'ember';
 const { get } = Ember;
 
 var nodeAjax = function(options) {
-  let httpRegex = /^https?:\/\//
+  let httpRegex = /^https?:\/\//;
+  let protocolRelativeRegex = /^\/\//;
+  let protocol = get(this, 'fastboot.request.protocol') + ':';
 
-  if (!httpRegex.test(options.url)) {
+  if (protocolRelativeRegex.test(options.url)) {
+    options.url = protocol + options.url;
+  } else if (!httpRegex.test(options.url)) {
     try {
-      options.url = get(this, 'fastboot.request.protocol') + '://' + get(this, 'fastboot.request.host') + options.url
+      options.url = protocol + '//' + get(this, 'fastboot.request.host') + options.url;
     } catch (fbError) {
       throw new Error('You are using Ember Data with no host defined in your adapter. This will attempt to use the host of the FastBoot request, which is not configured for the current host of this request. Please set the hostWhitelist property for in your environment.js. FastBoot Error: ' + fbError.message);
     }
