49fab54d23c6a491199413c8a334caf07e8ecbd5
Kit Sunde <kitsunde@gmail.com>
2016-12-01 10:34:05 +0800

    Only build commonjs.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,6 +2,7 @@ var alchemist = require('broccoli-module-alchemist');
 
 module.exports = function() {
   return alchemist({
-    entry: 'fastboot-app-server.js'
+    entry: 'fastboot-app-server.js',
+    targets: ['cjs']
   });
 };
