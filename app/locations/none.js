be8fdd61d178098848fbf86266036f04afe441b5
kellyselden <kellyselden@gmail.com>
2017-01-18 11:42:30 -0800

    path needs rootURL massaging earlier to pass the isTransitioning check

--- app/locations/none.js
+++ app/locations/none.js
@@ -27,19 +27,28 @@ export default Ember.NoneLocation.extend({
   _response: readOnly('fastboot.response'),
   _request: readOnly('fastboot.request'),
 
+  _normalizePath(path, isInitialPath) {
+    if (!isInitialPath) {
+        // only on redirects (transitionTo, replaceWith, etc...)
+        // incoming path does not include rootURL
+        let rootURL = get(this, 'rootURL');
+        rootURL = rootURL.substr(0, rootURL.length - 1);
+        path = `${rootURL}${path}`;
+    }
+    return path;
+  },
+
   setURL(path) {
     if (get(this, 'fastboot.isFastBoot')) {
       let currentPath = get(this, 'path');
       let isInitialPath = !currentPath || currentPath.length === 0;
+      path = this._normalizePath(path, isInitialPath);
       let isTransitioning = currentPath !== path;
       let response = get(this, '_response');
 
       if (isTransitioning && !isInitialPath) {
         let protocol = get(this, '_request.protocol');
         let host = get(this, '_request.host');
-        let rootURL = get(this, 'rootURL');
-        rootURL = rootURL.substr(0, rootURL.length - 1);
-        path = `${rootURL}${path}`;
         let redirectURL = `${protocol}://${host}${path}`;
 
         response.statusCode = this.get('_redirectCode');
