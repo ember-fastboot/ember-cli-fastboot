834e2a56f2dc4adb465828409fe45d501b2c291b
Josemar Luedke <josemarluedke@gmail.com>
2017-07-13 23:47:27 -0300

    Add fastboot-location-test back

--- test/fixtures/fastboot-location/config/environment.js
+++ test/fixtures/fastboot-location/config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     rootURL: '/my-root/',
+    locationType: 'auto',
     environment: environment,
     modulePrefix: 'fastboot-location',
     fastboot: {
