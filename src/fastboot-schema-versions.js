devotox/fastboot

Extend FastBoot.config to read other provided config as well.

src/fastboot-schema-versions.js
@@ -7,9 +7,10 @@
  * should be added in fastboot lib) everytime fastboot addon schema version is bumped.
  */
 const FastBootSchemaVersions = {
-  'latest': 2, // latest schema version supported by fastboot library
+  'latest': 3, // latest schema version supported by fastboot library
   'base': 1, // first schema version supported by fastboot library
-  'manifestFileArrays': 2 // schema version when app and vendor in manifest supported an array of files
+  'manifestFileArrays': 2, // schema version when app and vendor in manifest supported an array of files
+  'configExtension': 3 // schema version when FastBoot.config can read artibitary indexed config
 };
 
 module.exports = FastBootSchemaVersions;

ember-fastboot/ember-cli-fastboot

src/fastboot-schema-versions.js
@@ -7,9 +7,10 @@
  * should be added in fastboot lib) everytime fastboot addon schema version is bumped.
  */
 const FastBootSchemaVersions = {
-  'latest': 2, // latest schema version supported by fastboot library
+  'latest': 3, // latest schema version supported by fastboot library
   'base': 1, // first schema version supported by fastboot library
-  'manifestFileArrays': 2 // schema version when app and vendor in manifest supported an array of files
+  'manifestFileArrays': 2, // schema version when app and vendor in manifest supported an array of files
+  'configExtension': 3 // schema version when FastBoot.config can read arbitrary indexed config
 };
 
 module.exports = FastBootSchemaVersions;