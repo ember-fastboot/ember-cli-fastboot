6b4061522c199f639519fb3f9bff35df30b97d51
Kelly Selden <kellyselden@gmail.com>
2017-11-04 12:29:46 -0700

    fix eslint error

--- app/instance-initializers/clear-double-boot.js
+++ app/instance-initializers/clear-double-boot.js
@@ -1,5 +1,3 @@
-/*globals Ember*/
-
 // When using `ember serve` when fastboot addon is installed the application 
 // output will already be rendered to the DOM when the actual JavaScript 
 // loads. Ember does not automatically clear its `rootElement` so this 
