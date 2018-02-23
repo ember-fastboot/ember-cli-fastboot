577c62c6ff1637ed3b03c819c9b3d17104e74100
snewcomer <snewcomer24@gmail.com>
2018-02-23 07:19:02 -0800

    upgrade to 3.0

--- testem.js
+++ testem.js
@@ -11,12 +11,14 @@ module.exports = {
     Chrome: {
       mode: 'ci',
       args: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.TRAVIS ? '--no-sandbox' : null,
+
         '--disable-gpu',
         '--headless',
-        '--no-sandbox',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
-      ]
+      ].filter(Boolean)
     }
   }
 };
