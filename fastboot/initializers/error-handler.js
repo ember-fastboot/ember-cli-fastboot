8746def50813525cf63446d665aa80f304c53133
rhilliar <rhilliard@linkedin.com>
2019-05-03 11:12:13 -0700

    Fixing ember warning of Logger deprecation

--- fastboot/initializers/error-handler.js
+++ fastboot/initializers/error-handler.js
@@ -8,12 +8,12 @@ import Ember from 'ember';
 export default {
   name: 'error-handler',
 
-  initialize: function(application) {
+  initialize: function() {
     if (!Ember.onerror) {
       // if no onerror handler is defined, define one for fastboot environments
       Ember.onerror = function(err) {
-        let errorMessage = `There was an error running your app in fastboot. More info about the error: \n ${err.stack || err}`;
-        Ember.Logger.error(errorMessage);
+        const errorMessage = `There was an error running your app in fastboot. More info about the error: \n ${err.stack || err}`;
+        console.error(errorMessage);
       }
     }
   }
