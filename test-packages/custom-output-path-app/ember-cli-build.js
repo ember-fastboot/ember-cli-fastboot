f5934b9cb328fc4eff150d39d55ffd5d0b05c958
Dave Laird <kiwiupover@gmail.com>
2020-07-10 09:18:18 -0700

    update custom output tests

--- test-packages/custom-output-path-app/ember-cli-build.js
+++ test-packages/custom-output-path-app/ember-cli-build.js
@@ -4,11 +4,17 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    fingerprint: {
-      prepend: 'https://totally-sick-cdn.example.com/',
-      exclude: ['vendor.js', 'custom-output-path-app-fastboot.js'],
-      generateAssetMap: true,
-      assetMapPath: 'totally-customized-asset-map.json'
+    outputPaths: {
+      app: {
+        html: 'index.html',
+        css: {
+          'app': '/some-assets/path/app.css',
+        },
+        js: '/some-assets/path/app-file.js'
+      },
+      vendor: {
+        js: '/some-assets/path/lib.js'
+      }
     }
   });
 
--- test-packages/custom-output-path-app/test/build-output-test.js
+++ test-packages/custom-output-path-app/test/build-output-test.js
@@ -24,7 +24,7 @@ describe('generating package.json', function () {
       manifest.appFiles.forEach(function (file) {
         expect(`dist/${file}`).to.be.a.file();
       });
-      expect(p(manifest.htmlFile)).to.be.a.file();
+      expect(`dist/${manifest.htmlFile}`).to.be.a.file();
 
       expect(manifest.vendorFiles).to.include('some-assets/path/lib.js');
       manifest.vendorFiles.forEach(function (file) {
@@ -32,29 +32,4 @@ describe('generating package.json', function () {
       });
     });
   });
-
-  // describe('with custom htmlFile', function () {
-  //   this.timeout(300000);
-
-  //   let customApp = new AddonTestApp();
-
-  //   before(function () {
-  //     return customApp.create('custom-html-file')
-  //       .then(function () {
-  //         return customApp.runEmberCommand('build', '--environment=production');
-  //       });
-  //   });
-
-  //   it('uses custom htmlFile in the manifest', function () {
-  //     function p(filePath) {
-  //       return customApp.filePath(path.join('dist', filePath));
-  //     }
-
-  //     let pkg = fs.readJsonSync(customApp.filePath('/dist/package.json'));
-  //     let manifest = pkg.fastboot.manifest;
-
-  //     expect(manifest.htmlFile).to.equal('custom-index.html');
-  //     expect(p(manifest.htmlFile)).to.be.a.file();
-  //   });
-  // });
 });
