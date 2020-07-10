12067a4ff5acf5b7b08481130b6b85e427f30f62
Dave Laird <kiwiupover@gmail.com>
2020-07-10 08:18:10 -0700

    cleaned up test code

--- test-packages/basic-app/test/package-json-test.js
+++ test-packages/basic-app/test/package-json-test.js
@@ -8,7 +8,7 @@ const execa = require("execa");
 chai.use(require('chai-fs'));
 
 describe('generating package.json', function () {
-  this.timeout(300000);
+  this.timeout(50000);
 
   describe('with FastBoot builds', function () {
 
@@ -133,41 +133,6 @@ describe('generating package.json', function () {
 
       expect(pkg.fastboot.config['foo']).to.equal('bar');
     });
-
-    // describe('with addon that implements fastbootConfigTree', function () {
-    //   let app;
-
-    //   before(function () {
-    //     app = new AddonTestApp();
-
-    //     return app.create('fastboot-config', {
-    //       skipNpm: true
-    //     })
-    //       .then(addFastBootDeps)
-    //       .then(function () {
-    //         return app.run('npm', 'install');
-    //       })
-    //       .then(function () {
-    //         return app.runEmberCommand('build');
-    //       });
-    //   });
-
-    //   it('it extends the application config', function () {
-    //     let pkg = fs.readJsonSync(app.filePath('dist/package.json'));
-
-    //     expect(pkg.fastboot.config['fastboot-config']).to.deep.equal({
-    //       foo: 'bar',
-    //       modulePrefix: 'fastboot-config',
-    //       environment: 'development',
-    //       baseURL: '/',
-    //       locationType: 'auto',
-    //       EmberENV: { FEATURES: {} },
-    //       APP: { name: 'fastboot-config', version: '0.0.0', autoboot: false },
-    //       fastboot: { hostWhitelist: ['example.com', 'subdomain.example.com', '/localhost:\\d+/'] },
-    //       exportApplicationGlobal: true
-    //     });
-    //   });
-    // });
   });
 
   describe('with production FastBoot builds', function () {
@@ -191,104 +156,4 @@ describe('generating package.json', function () {
       });
     });
   });
-
-  // describe('with customized fingerprinting options', function () {
-  //   // Tests an app with a custom `assetMapPath` set
-  //   let customApp = new AddonTestApp();
-
-  //   before(function () {
-  //     return customApp.create('customized-fingerprinting')
-  //       .then(function () {
-  //         return customApp.runEmberCommand('build', '--environment=production');
-  //       });
-  //   });
-
-  //   it('respects a custom asset map path and prepended URLs', function () {
-  //     expect(customApp.filePath('dist/totally-customized-asset-map.json')).to.be.a.file();
-
-  //     function p(filePath) {
-  //       return customApp.filePath(path.join('dist', filePath));
-  //     }
-
-  //     let pkg = fs.readJsonSync(customApp.filePath('/dist/package.json'));
-  //     let manifest = pkg.fastboot.manifest;
-
-  //     manifest.appFiles.forEach(function (file) {
-  //       expect(p(file)).to.be.a.file();
-  //     });
-  //     expect(p(manifest.htmlFile)).to.be.a.file();
-  //     manifest.vendorFiles.forEach(function (file) {
-  //       expect(p(file)).to.be.a.file();
-  //     });
-  //   });
-
-  //   it('respects individual files being excluded from fingerprinting', function () {
-  //     expect(customApp.filePath('dist/totally-customized-asset-map.json')).to.be.a.file();
-
-  //     let pkg = fs.readJsonSync(customApp.filePath('/dist/package.json'));
-  //     let manifest = pkg.fastboot.manifest;
-
-  //     // customized-fingerprinting-fastboot.js is excluded from fingerprinting
-  //     expect(manifest.appFiles).to.include('assets/customized-fingerprinting-fastboot.js');
-  //     // vendor.js is excluded from fingerprinting
-  //     expect(manifest.vendorFiles).to.include('assets/vendor.js');
-  //   });
-  // });
-
-  // describe('with customized outputPaths options', function () {
-  //   // Tests an app with a custom `outputPaths` set
-  //   let customApp = new AddonTestApp();
-
-  //   before(function () {
-  //     return customApp.create('customized-outputpaths')
-  //       .then(function () {
-  //         return customApp.runEmberCommand('build');
-  //       });
-  //   });
-
-  //   it('respects custom output paths and maps to them in the manifest', function () {
-  //     function p(filePath) {
-  //       return customApp.filePath(path.join('dist', filePath));
-  //     }
-
-  //     let pkg = fs.readJsonSync(customApp.filePath('/dist/package.json'));
-  //     let manifest = pkg.fastboot.manifest;
-
-  //     expect(manifest.appFiles).to.include('some-assets/path/app-file.js');
-  //     manifest.appFiles.forEach(function (file) {
-  //       expect(p(file)).to.be.a.file();
-  //     });
-  //     expect(p(manifest.htmlFile)).to.be.a.file();
-
-  //     expect(manifest.vendorFiles).to.include('some-assets/path/lib.js');
-  //     manifest.vendorFiles.forEach(function (file) {
-  //       expect(p(file)).to.be.a.file();
-  //     });
-  //   });
-  // });
-
-  // describe('with custom htmlFile', function () {
-  //   this.timeout(300000);
-
-  //   let customApp = new AddonTestApp();
-
-  //   before(function () {
-  //     return customApp.create('custom-html-file')
-  //       .then(function () {
-  //         return customApp.runEmberCommand('build', '--environment=production');
-  //       });
-  //   });
-
-  //   it('uses custom htmlFile in the manifest', function () {
-  //     function p(filePath) {
-  //       return customApp.filePath(path.join('dist', filePath));
-  //     }
-
-  //     let pkg = fs.readJsonSync(customApp.filePath('/dist/package.json'));
-  //     let manifest = pkg.fastboot.manifest;
-
-  //     expect(manifest.htmlFile).to.equal('custom-index.html');
-  //     expect(p(manifest.htmlFile)).to.be.a.file();
-  //   });
-  // });
 });
