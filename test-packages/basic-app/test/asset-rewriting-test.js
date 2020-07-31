576b9b12150bdb76077ae5f262098497f4d2da53
Dave Laird <kiwiupover@gmail.com>
2020-07-31 08:04:50 -0700

    bumped timeout:

--- test-packages/basic-app/test/asset-rewriting-test.js
+++ test-packages/basic-app/test/asset-rewriting-test.js
@@ -6,7 +6,7 @@ const execa = require("execa");
 chai.use(require('chai-fs'));
 
 describe.only('rewriting HTML', function () {
-  this.timeout(50000);
+  this.timeout(100000);
 
   it('builds an index.html that points to the browser build', async function () {
 
