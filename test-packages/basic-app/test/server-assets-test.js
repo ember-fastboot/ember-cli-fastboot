34fb86687338e1e8cfd5ba604a05cadb64223bdd
Dave Laird <kiwiupover@gmail.com>
2020-09-10 07:34:45 -0700

    adding server-assets test to basic app

---
+++ test-packages/basic-app/test/server-assets-test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const RSVP = require('rsvp');
+const request = RSVP.denodeify(require('request'));
+const expect = require('chai').use(require('chai-string')).expect;
+const { startServer, stopServer } = require('../../test-libs/index');
+
+describe.only('serve assets acceptance', function() {
+  this.timeout(20000);
+
+  before(function() {
+    return startServer({
+      command: 'serve'
+    });
+
+  });
+
+  after(function() {
+    return stopServer();
+  });
+
+  it('/assets/vendor.js', async () => {
+    const response = await request('http://localhost:49741/assets/vendor.js')
+
+    expect(response.statusCode).to.equal(200);
+    expect(response.headers["content-type"]).to.equalIgnoreCase("application/javascript; charset=utf-8");
+    expect(response.body).to.contain("Ember =");
+  });
+
+  it('/assets/basic-app.js', async () => {
+    const response = await request('http://localhost:49741/assets/basic-app.js')
+
+    expect(response.statusCode).to.equal(200);
+    expect(response.headers["content-type"]).to.equalIgnoreCase("application/javascript; charset=utf-8");
+    expect(response.body).to.contain("this.route('posts')");
+  });
+});
