f89a1f2870bf8dea7a2ddbce9648124765d71eaa
Dave Laird <kiwiupover@gmail.com>
2020-09-10 07:35:15 -0700

    remove dummy fixture

--- packages/ember-cli-fastboot/test/fixtures/dummy/app/routes/imports.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model: function() {
-    return {
-      importStatus: window.browserImportStatus || 'FastBoot default default value'
-    };
-  }
-});
