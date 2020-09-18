74b29f19aeb676ccfd621883f674f0e128984f6d
Chris Manson <chris@manson.ie>
2020-09-18 16:29:53 +0100

    wip more stuff

--- packages/ember-cli-fastboot/test/default-blueprint-test.js
+++ packages/ember-cli-fastboot/test/default-blueprint-test.js
@@ -1,23 +1,44 @@
 'use strict';
 
 const blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
-const setupTestHooks = blueprintHelpers.setupTestHooks;
-const emberNew = blueprintHelpers.emberNew;
-const emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+
+const { emberGenerate, emberNew, setupTestHooks } = blueprintHelpers;
+
+const MockUI = require('console-ui/mock');
 
 const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
 
-describe('Acceptance: ember generate and destroy default-blueprint', function() {
+describe.only('Acceptance: ember generate and destroy default-blueprint', function() {
   setupTestHooks(this);
 
   it('default-blueprint foo', async function() {
     let args = ['ember-cli-fastboot'];
 
+    // process.stdout.on('data', function(data) {
+    //   console.log('face', data.toString());
+    //   process.stdin.writeLine('y');
+    //   // process.stdout.write(data);
+    // });
+
+
+
     // pass any additional command line options in the arguments array
     await emberNew();
 
-    const file = await emberGenerateDestroy(args);
+    // let ui = new MockUI();
 
-    expect(file('config/targets.js')).to.contain(`node: 'current'`);
+    // let interval = setInterval(() => {
+    //   ui.inputStream.write('y\n\r');
+    // }, 2000);
+
+    const ember = await emberGenerate(args);
+
+    setTimeout(() => {
+      ember.inputStream.write('y\n');
+    }, 4000)
+
+    // clearInterval(interval);
+
+    // expect(file('config/targets.js')).to.contain(`node: 'current'`);
   });
 });
