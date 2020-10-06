d73bda3a5cf4951f87e9f23633e0b76f6d885fab
Dave Laird <kiwiupover@gmail.com>
2020-10-05 22:03:06 -0700

    alignment

--- test-packages/basic-app/app/instance-initializers/initializer.js
+++ test-packages/basic-app/app/instance-initializers/initializer.js
@@ -2,6 +2,6 @@ export default {
   name: 'instant-initialize',
   initialize() {
     return `But how do you do? I see I have frightened
-          you--sit down and tell me all the news.`
+            you--sit down and tell me all the news.`
   }
 }
--- test-packages/basic-app/fastboot/instance-initializers/initializer.js
+++ test-packages/basic-app/fastboot/instance-initializers/initializer.js
@@ -2,6 +2,6 @@ export default {
   name: 'instant-initialize',
   initialize() {
     return `It was in July, 1805, and the speaker was the well-known Anna Pavlovna
-        Scherer, maid of honor and favorite of the Empress Marya Fedorovna.`
+            Scherer, maid of honor and favorite of the Empress Marya Fedorovna.`
   }
 }
