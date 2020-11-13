ce68318551d605394dbbda09f5d17edf801e687d
Dave Laird <kiwiupover@gmail.com>
2020-11-13 09:41:59 -0800

    adding custom fastboot app

--- test-packages/custom-fastboot-app/config/environment.js
+++ test-packages/custom-fastboot-app/config/environment.js
@@ -6,6 +6,11 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+
+    fastboot: {
+      htmlFile: 'custom-index.html'
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
--- test-packages/custom-fastboot-app/ember-cli-build.js
+++ test-packages/custom-fastboot-app/ember-cli-build.js
@@ -4,21 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      prepend: 'https://totally-sick-cdn.example.com/',
+      exclude: ['vendor.js', 'custom-fastboot-app.js'],
+      generateAssetMap: true,
+      assetMapPath: 'totally-customized-asset-map.json'
+    }
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   return app.toTree();
 };
