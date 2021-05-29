4fd0dc4c26141c6aff1b8313bac06cfc0a7624e9
ankushdharkar <ankushdharkar@gmail.com>
2021-05-29 13:00:31 -0700

    Testing ember-cli-fastboot-testing in new test-package #831

---
+++ test-packages/custom-fastboot-testing-app/tests/fastboot/home-page-test.js
@@ -0,0 +1,11 @@
+import { module, test } from 'qunit';
+import { setup, visit } from 'ember-cli-fastboot-testing/test-support';
+
+module('FastBoot | home-page', function(hooks) {
+  setup(hooks);
+
+  test('it renders a page...', async function(assert) {
+    await visit('/');
+    assert.dom('[data-test-welcome-header]').hasText('Welcome to Ember');
+  });
+});
