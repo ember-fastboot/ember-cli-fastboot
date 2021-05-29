c9a3b430a32b5409ef5e1df616cc144905d17af4
ankushdharkar <ankushdharkar@gmail.com>
2021-05-29 15:20:55 -0700

    welcome-page test #831

--- test-packages/ember-cli-fastboot-testing-app/tests/fastboot/welcome-page-test.js
+++ test-packages/ember-cli-fastboot-testing-app/tests/fastboot/welcome-page-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setup, visit } from 'ember-cli-fastboot-testing/test-support';
 
-module('FastBoot | home-page', function(hooks) {
+module('FastBoot | welcome-page', function(hooks) {
   setup(hooks);
 
   test('it renders the welcome page', async function(assert) {
