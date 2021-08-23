54b84c93883e6cd309e65058554d7c2ef12ed38e
Scott Newcomer <snewcomer24@gmail.com>
2021-08-22 22:49:34 -0500

    undo this

--- test-packages/ember-cli-fastboot-testing-app/config/targets.js
+++ test-packages/ember-cli-fastboot-testing-app/config/targets.js
@@ -1,10 +1,17 @@
 'use strict';
 
 const browsers = [
-  'last 2 Chrome versions',
+  'last 1 Chrome versions',
   'last 1 Firefox versions',
   'last 1 Safari versions'
 ];
+
+const isCI = !!process.env.CI;
+const isProduction = process.env.EMBER_ENV === 'production';
+
+if (isCI || isProduction) {
+  browsers.push('ie 11');
+}
 
 module.exports = {
   browsers
