42c55917617e3c19ad29dd7c73fcd54402f24ddd
Edward Faulkner <ef@alum.mit.edu>
2021-09-10 15:07:41 -0400

    Remove implicit injection
    This is required to clear the implicit-injection deprecation on Ember 3.27.
    
    This may need to be released as a semver major because this version of `fastboot` will only work with versions of `ember-cli-fastboot` after https://github.com/ember-fastboot/ember-cli-fastboot/pull/841

--- packages/fastboot/src/fastboot-info.js
+++ packages/fastboot/src/fastboot-info.js
@@ -38,6 +38,5 @@ module.exports = class FastBootInfo {
    */
   register(instance) {
     instance.register('info:-fastboot', this, { instantiate: false });
-    instance.inject('service:fastboot', '_fastbootInfo', 'info:-fastboot');
   }
 };
