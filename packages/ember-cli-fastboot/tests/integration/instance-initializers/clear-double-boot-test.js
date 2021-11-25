b507ca3097015a3c278e4eddc40004b62a52efb4
Sergey Astapov <sergeastapov@gmail.com>
2021-11-25 18:36:13 -0500

    Make edition=octane in ember-cli-fastboot for Ember v4 compatibility

--- packages/ember-cli-fastboot/tests/integration/instance-initializers/clear-double-boot-test.js
+++ packages/ember-cli-fastboot/tests/integration/instance-initializers/clear-double-boot-test.js
@@ -21,11 +21,13 @@ module('Instance-initializer: clear-double-boot', function(hooks) {
 
   test('It can handle bad markup', async function(assert) {
     this.set('BAD_HTML', `
-      <script type="x/boundary" id="fastboot-body-start"></script>
-      <div id="content-in-between">
-        <em><em>
+      <div class="wrapper">
+        <script type="x/boundary" id="fastboot-body-start"></script>
+        <div id="content-in-between">
+          <em><em>
+        </div>
+        <script type="x/boundary" id="fastboot-body-end"></script>
       </div>
-      <script type="x/boundary" id="fastboot-body-end"></script>
     `);
 
     // render the whole tree dynamically to more closely mimc bad markup cases
