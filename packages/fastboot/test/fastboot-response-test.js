ee411a135a5057ee4c936e350f1776a9ad91c6cc
Nathaniel Furniss <nlfurniss@gmail.com>
2023-02-09 11:35:00 -0800

    fix prettier errors

--- packages/fastboot/test/fastboot-response-test.js
+++ packages/fastboot/test/fastboot-response-test.js
@@ -2,10 +2,10 @@ var expect = require('chai').expect;
 var FastBootHeaders = require('./../src/fastboot-headers.js');
 var FastBootResponse = require('./../src/fastboot-response.js');
 
-describe('FastBootResponse', function() {
+describe('FastBootResponse', function () {
   var fastBootResponse;
 
-  beforeEach(function() {
+  beforeEach(function () {
     var mockResponse = {
       getHeaders() {
         return {
@@ -18,27 +18,27 @@ describe('FastBootResponse', function() {
     fastBootResponse = new FastBootResponse(mockResponse);
   });
 
-  describe('headers', function() {
-    it('should have headers', function() {
+  describe('headers', function () {
+    it('should have headers', function () {
       expect(fastBootResponse).to.have.ownProperty('headers');
     });
 
-    it('should be an instance of FastBootHeaders', function() {
+    it('should be an instance of FastBootHeaders', function () {
       expect(fastBootResponse.headers).to.be.an.instanceOf(FastBootHeaders);
     });
 
-    it("should contain the original response's headers", function() {
+    it("should contain the original response's headers", function () {
       var header = fastBootResponse.headers.getAll('i-am-a');
       expect(header).to.deep.equal(['mock header', 'me too']);
     });
   });
 
-  describe('statusCode', function() {
-    it('should have a statusCode', function() {
+  describe('statusCode', function () {
+    it('should have a statusCode', function () {
       expect(fastBootResponse).to.have.ownProperty('statusCode');
     });
 
-    it('should default to 200', function() {
+    it('should default to 200', function () {
       expect(fastBootResponse.statusCode).to.equal(200);
     });
   });
