ad4911cd27ed94627b9915e84fa3852525fcd09a
Chris Manson <chris@manson.ie>
2023-09-01 16:35:21 +0100

    move a shoebox test out of pre-built fixtures

--- test-packages/test-scenarios/fastboot-shoebox-test.mjs
+++ test-packages/test-scenarios/fastboot-shoebox-test.mjs
@@ -3,25 +3,139 @@
 import { baseApp } from './scenarios.mjs';
 import { Scenarios } from 'scenario-tester';
 import qunit from 'qunit';
+import { join } from 'path';
+import FastBoot from 'fastboot';
+import lodash from 'lodash';
+import { expect } from 'chai';
 
+const { merge } = lodash;
 const { module: Qmodule, test } = qunit;
 
+async function buildFastboot(app) {
+  let result = await app.execute(`node node_modules/ember-cli/bin/ember build`);
+
+  if (result.exitCode !== 0) {
+    throw new Error(`failed to build app for fastboot: ${result.output}`);
+  }
+
+  return new FastBoot({
+    distPath: join(app.dir, 'dist'),
+    resilient: false,
+  });
+}
+
 Scenarios.fromProject(baseApp)
-  .map('fastboot-shoebox-test', project => {
-    // TODO add some specifics here
+  .map('fastboot-shoebox-test', (project) => {
+    merge(project.files, {
+      app: {
+        routes: {
+          'application.js': `import Route from '@ember/routing/route';
+          import { inject as service } from '@ember/service'
+
+          export default class ApplicationRoute extends Route {
+            @service fastboot;
+
+            model() {
+              const fastboot = this.fastboot;
+              var shoebox = fastboot.shoebox;
+              if (fastboot.isFastBoot) {
+                shoebox.put('key1', { foo: 'bar' });
+                shoebox.put('key2', { zip: 'zap' });
+                shoebox.put('key3', { htmlSpecialCase: 'R&B > Jazz' });
+                shoebox.put('key4', { nastyScriptCase: "<script>alert('owned');</script></script></script>" });
+                shoebox.put('key5', { otherUnicodeChars: '&&>><<\u2028\u2028\u2029\u2029' });
+              }
+            }
+          }`,
+        },
+      },
+    });
   })
-  .forEachScenario(scenario => {
+  .forEachScenario((scenario) => {
     Qmodule(scenario.name, function (hooks) {
       let app; // PreparedApp
+      let fastboot;
 
       hooks.before(async () => {
         app = await scenario.prepare();
-        // any custom setup that you have for each scenario  
+        fastboot = await buildFastboot(app);
       });
 
-      test('it works', async function (assert) {
-        // your custom test code
-        assert.ok(true);
+      test('can render the escaped shoebox HTML', async function (assert) {
+        // we ported the tests directly from mocha so we're re-using the expect style
+        assert.expect(0);
+
+        const html = await fastboot.visit('/').then((r) => r.html());
+
+        expect(html).to.match(
+          /<script type="fastboot\/shoebox" id="shoebox-key1">{"foo":"bar"}<\/script>/
+        );
+        expect(html).to.match(
+          /<script type="fastboot\/shoebox" id="shoebox-key2">{"zip":"zap"}<\/script>/
+        );
+
+        // Special characters are JSON encoded, most notably the </script sequence.
+        expect(html).to.include(
+          '<script type="fastboot/shoebox" id="shoebox-key4">{"nastyScriptCase":"\\u003cscript\\u003ealert(\'owned\');\\u003c/script\\u003e\\u003c/script\\u003e\\u003c/script\\u003e"}</script>'
+        );
+
+        expect(html).to.include(
+          '<script type="fastboot/shoebox" id="shoebox-key5">{"otherUnicodeChars":"\\u0026\\u0026\\u003e\\u003e\\u003c\\u003c\\u2028\\u2028\\u2029\\u2029"}</script>'
+        );
+      });
+
+      test('can render the escaped shoebox HTML with shouldRender set to false', async function (assert) {
+        // we ported the tests directly from mocha so we're re-using the expect style
+        assert.expect(0);
+
+        const html = await fastboot
+          .visit('/', {
+            shouldRender: false,
+          })
+          .then((r) => r.html());
+
+        expect(html).to.match(
+          /<script type="fastboot\/shoebox" id="shoebox-key1">{"foo":"bar"}<\/script>/
+        );
+        expect(html).to.match(
+          /<script type="fastboot\/shoebox" id="shoebox-key2">{"zip":"zap"}<\/script>/
+        );
+
+        // Special characters are JSON encoded, most notably the </script sequence.
+        expect(html).to.include(
+          '<script type="fastboot/shoebox" id="shoebox-key4">{"nastyScriptCase":"\\u003cscript\\u003ealert(\'owned\');\\u003c/script\\u003e\\u003c/script\\u003e\\u003c/script\\u003e"}</script>'
+        );
+
+        expect(html).to.include(
+          '<script type="fastboot/shoebox" id="shoebox-key5">{"otherUnicodeChars":"\\u0026\\u0026\\u003e\\u003e\\u003c\\u003c\\u2028\\u2028\\u2029\\u2029"}</script>'
+        );
+      });
+
+      test('cannot render the escaped shoebox HTML when disableShoebox is set to true', async function (assert) {
+        // we ported the tests directly from mocha so we're re-using the expect style
+        assert.expect(0);
+
+        const html = await fastboot
+          .visit('/', {
+            disableShoebox: true,
+          })
+          .then((r) => r.html());
+
+        expect(html).to.not.match(
+          /<script type="fastboot\/shoebox" id="shoebox-key1">{"foo":"bar"}<\/script>/
+        );
+        expect(html).to.not.match(
+          /<script type="fastboot\/shoebox" id="shoebox-key2">{"zip":"zap"}<\/script>/
+        );
+
+        // Special characters are JSON encoded, most notably the </script sequence.
+        expect(html).to.not.include(
+          '<script type="fastboot/shoebox" id="shoebox-key4">{"nastyScriptCase":"\\u003cscript\\u003ealert(\'owned\');\\u003c/script\\u003e\\u003c/script\\u003e\\u003c/script\\u003e"}</script>'
+        );
+
+        expect(html).to.not.include(
+          '<script type="fastboot/shoebox" id="shoebox-key5">{"otherUnicodeChars":"\\u0026\\u0026\\u003e\\u003e\\u003c\\u003c\\u2028\\u2028\\u2029\\u2029"}</script>'
+        );
       });
     });
   });
