93a4aad4a3e42a281e88af8f9d882d422496c612
Gaurav Munjal <gaurav.munjal@civicplus.com>
2023-10-17 11:29:49 -0400

    fix ember-cli-fastboot-testing-app test

--- test-packages/ember-cli-fastboot-testing-app/tests/test-helper.js
+++ test-packages/ember-cli-fastboot-testing-app/tests/test-helper.js
@@ -2,6 +2,10 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
