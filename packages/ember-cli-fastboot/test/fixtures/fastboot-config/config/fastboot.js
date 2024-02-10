c398422ac056dc3a0104fe458701b743da203680
Chris Manson <chris@manson.ie>
2024-02-10 16:06:03 +0000

    move fastboot-config-test to scenario tester

--- packages/ember-cli-fastboot/test/fixtures/fastboot-config/config/fastboot.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-undef, no-unused-vars, prettier/prettier */
-module.exports = function(environment) {
-  return {
-    sandboxGlobals: {
-      myGlobal: 'My Global'
-    }
-  };
-}
