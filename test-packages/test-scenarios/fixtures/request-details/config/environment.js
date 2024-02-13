c2699efc8068879ebcc7e55b681d4987705e0127
Chris Manson <chris@manson.ie>
2024-02-13 22:31:00 +0000

    move request-details test to scenario-tester

--- test-packages/test-scenarios/fixtures/request-details/config/environment.js
+++ test-packages/test-scenarios/fixtures/request-details/config/environment.js
@@ -4,7 +4,7 @@
 
 module.exports = function (environment) {
   let ENV = {
-    modulePrefix: 'request',
+    modulePrefix: 'classic-app-template',
     environment,
     rootURL: '/',
     locationType: 'auto',
