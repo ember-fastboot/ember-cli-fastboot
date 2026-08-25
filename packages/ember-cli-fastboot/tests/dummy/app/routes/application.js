3b100791ffa461fdf46967c9d1a76afe5b470f25
Chris Manson <chris@manson.ie>
2026-08-26 00:21:35 +0100

    fix service deprecation in dummy app

--- packages/ember-cli-fastboot/tests/dummy/app/routes/application.js
+++ packages/ember-cli-fastboot/tests/dummy/app/routes/application.js
@@ -1,10 +1,10 @@
 /* eslint-disable ember/no-classic-classes, ember/no-get */
 import Route from '@ember/routing/route';
-import { inject } from '@ember/service';
+import { service } from '@ember/service';
 import { readOnly } from '@ember/object/computed';
 
 export default Route.extend({
-  fastboot: inject(),
+  fastboot: service(),
   shoebox: readOnly('fastboot.shoebox'),
 
   model() {
