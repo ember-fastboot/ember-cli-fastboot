7123e42bd8f1d3b7fadd049ba9953d2ebbd6ac37
Chris Manson <chris@manson.ie>
2026-09-03 11:17:34 +0100

    remove use of Ember.get in ajax initialiser

--- packages/ember-cli-fastboot/fastboot/initializers/ajax.js
+++ packages/ember-cli-fastboot/fastboot/initializers/ajax.js
@@ -1,20 +1,15 @@
-/* eslint-disable ember/new-module-imports */
 /* globals najax */
-import Ember from 'ember';
-
-const { get } = Ember;
-
 var nodeAjax = function (options) {
   let httpRegex = /^https?:\/\//;
   let protocolRelativeRegex = /^\/\//;
-  let protocol = get(this, 'fastboot.request.protocol');
+  let protocol = this?.fastboot?.request?.protocol;
 
   if (protocolRelativeRegex.test(options.url)) {
     options.url = protocol + options.url;
   } else if (!httpRegex.test(options.url)) {
     try {
       options.url =
-        protocol + '//' + get(this, 'fastboot.request.host') + options.url;
+        protocol + '//' + this?.fastboot?.request?.host + options.url;
     } catch (fbError) {
       throw new Error(
         'You are using Ember Data with no host defined in your adapter. This will attempt to use the host of the FastBoot request, which is not configured for the current host of this request. Please set the hostWhitelist property for in your environment.js. FastBoot Error: ' +
