f5706454496b84c7f21ff0b8e8b79c0983def79c
Chris Manson <chris@manson.ie>
2026-09-06 00:50:19 +0100

    add a basic render rehydrate test

--- test-packages/test-scenarios/ember-cli-fastboot-test.mjs
+++ test-packages/test-scenarios/ember-cli-fastboot-test.mjs
@@ -39,34 +39,48 @@ appScenarios
   })
   .forEachScenario((scenario) => {
     Qmodule(scenario.name, function (hooks) {
-      let app; // PreparedApp
-      let process;
+      ['default', 'rehydrate'].forEach((renderMode) => {
+        let app; // PreparedApp
+        let process;
 
-      hooks.before(async () => {
-        app = await scenario.prepare();
-        process = await emberServe(app);
-      });
-
-      hooks.after(() => {
-        return process.stop();
-      });
-
-      test('provides sandbox globals', async function (assert) {
-        const response = await fetch(`http://localhost:${process.port}/`, {
-          headers: {
-            Accept: 'text/html',
-          },
+        hooks.before(async () => {
+          app = await scenario.prepare();
         });
 
-        assert.equal(response.status, 200, 'Status should be ok');
-        assert.equal(
-          response.headers.get('content-type'),
-          'text/html; charset=utf-8',
-          'headers should be right'
-        );
+        hooks.after(() => {
+          return process.stop();
+        });
 
-        const body = await response.text();
-        assert.matches(body, /<h1>My Global<\/h1>/);
+        test(`provides sandbox globals with ender mode ${renderMode}`, async function (assert) {
+          process = await emberServe(
+            app,
+            renderMode === 'rehydrate'
+              ? {
+                  EXPERIMENTAL_RENDER_MODE_SERIALIZE: 'true',
+                }
+              : {}
+          );
+
+          const response = await fetch(`http://localhost:${process.port}/`, {
+            headers: {
+              Accept: 'text/html',
+            },
+          });
+
+          assert.equal(response.status, 200, 'Status should be ok');
+          assert.equal(
+            response.headers.get('content-type'),
+            'text/html; charset=utf-8',
+            'headers should be right'
+          );
+
+          const body = await response.text();
+          if (renderMode === 'rehydrate') {
+            assert.matches(body, /-->My Global<!--/);
+          } else {
+            assert.matches(body, /<h1>My Global<\/h1>/);
+          }
+        });
       });
     });
   });
